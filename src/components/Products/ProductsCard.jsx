1be3a663f8c5fe513789b5601ab33fb3c754a878
celia22 <celialopez22@gmail.com>
2021-07-19 15:53:47 +0200

    fix bug when render products

--- src/components/Products/ProductsCard.jsx
+++ src/components/Products/ProductsCard.jsx
@@ -3,44 +3,42 @@ import React, { Component } from 'react';
 // import './SpaceCard.css';
 
 class ProductsCard extends Component {
-  constructor(props){
-    super(props)
-    this.state={
-      products: []
-    }
-  }
-
-	componentDidMount(){
-		this.setState({
-			products: this.props.products
-		})
+	constructor(props) {
+		super(props);
+		this.state = {
+			products: [],
+		};
 	}
 
-render(){
-  const { products } = this.state
-console.log("prod card", products)
-  	return (
-		<>
-			<h2>Products</h2>
-			<div className="space_card_scroll">
-				{products.map(item => {
-					return (
-						<>
-							<div key={item._id} className="product_card_item ">
+	componentDidUpdate(prevProps) {
+		if (this.state.products !== prevProps.products) {
+			this.setState({
+				products: this.props.products,
+			});
+		}
+	}
+
+	render() {
+		const { products } = this.state;
+		console.log('prod card', products);
+		return (
+			<>
+				<h2>Products</h2>
+				<div className="space_card_scroll">
+					{products.map((item, index) => {
+						return (
+							<div key={index} className="product_card_item ">
 								<h4>SpaceName: {item.spaceName}</h4>
-									<h4>Description: {item.description}</h4>
-									<h5>Price: {item.price}</h5>
-									<h5>Amount: {item.amount}</h5>
+								<h4>Description: {item.description}</h4>
+								<h5>Price: {item.price}</h5>
+								<h5>Amount: {item.amount}</h5>
 							</div>
-						</>
-					);
-				})}
-			</div>
-		</>
-	);
-
+						);
+					})}
+				</div>
+			</>
+		);
+	}
 }
-
-};
 
 export default ProductsCard;
--- src/components/space/SpacesCards.jsx
+++ src/components/space/SpacesCards.jsx
@@ -5,26 +5,23 @@ import './SpaceCard.css';
 const SpacesCards = props => {
 	const allSpaces = props.allSpaces;
 
-
 	return (
 		<>
 			<h2>Our spaces</h2>
 			<div className="space_card_scroll">
 				{allSpaces.map(item => {
 					return (
-						<>
-							<div key={item._id} className="space_card_item ">
-								<Link to={`/space/${item._id}/details`}>
-									<h4>Name: {item.spaceName}</h4>
-									<h4>Type: {item.spaceType}</h4>
-									<h4>Image: {item.imageUrlSpace}</h4>
-									<h4>Price:</h4>
-									<h5>Daily: {item.price.daily}</h5>
-									<h5>Weekly: {item.price.weekly}</h5>
-									<h5>Monthly: {item.price.monthly}</h5>
-								</Link>
-							</div>
-						</>
+						<div key={item._id} className="space_card_item ">
+							<Link to={`/space/${item._id}/details`}>
+								<h4>Name: {item.spaceName}</h4>
+								<h4>Type: {item.spaceType}</h4>
+								<h4>Image: {item.imageUrlSpace}</h4>
+								<h4>Price:</h4>
+								<h5>Daily: {item.price.daily}</h5>
+								<h5>Weekly: {item.price.weekly}</h5>
+								<h5>Monthly: {item.price.monthly}</h5>
+							</Link>
+						</div>
 					);
 				})}
 			</div>
